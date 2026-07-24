0b7e8e639420177c5da7163b0bf153926da7d899
Yuhao-Su931 <707947821@qq.com>
2026-07-24 14:36:41 +0800

    Run minute-source coverage probe

--- validate_t1_v3.py
+++ validate_t1_v3.py
@@ -5,7 +5,7 @@ def secid(code):
     sym, ex = code.split('.')
     return ('1.' if ex=='SH' else '0.') + sym
 url='https://push2his.eastmoney.com/api/qt/stock/trends2/get'
-p={'secid':secid('600992.SH'),'ndays':10,'iscr':0,'fields1':'f1,f2,f3,f4,f5,f6,f7,f8,f9,f10,f11,f12,f13','fields2':'f51,f52,f53,f54,f55,f56,f57,f58'}
+p={'secid':secid('600992.SH'),'ndays':10,'iscr':0,'iscca':0,'fields1':'f1,f2,f3,f4,f5,f6,f7,f8,f9,f10,f11,f12,f13','fields2':'f51,f52,f53,f54,f55,f56,f57,f58'}
 r=requests.get(url,params=p,timeout=30,headers={'User-Agent':'Mozilla/5.0','Referer':'https://quote.eastmoney.com/'})
 obj={'status':r.status_code,'url':r.url,'text_head':r.text[:200]}
 try:
